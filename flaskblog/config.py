94c74514ce72cb39e7a3668ffe37910827baf279
steffy.inexture <steffy.inexture@gmail.com>
2022-06-03 11:23:47 +0530

    update requirements

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,13 +1,14 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-
     MAIL_SERVER= 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    # app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-    # app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-    MAIL_USERNAME = 'steffy.inexture@gmail.com'
-    MAIL_PASSWORD = 'steffy!p@7069214086'
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
