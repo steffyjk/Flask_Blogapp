7500d23b2282a5599c2e9a2c0e618f92aa19208c
steffy.inexture <steffy.inexture@gmail.com>
2022-06-05 00:31:25 +0530

    updated Database URL

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -7,9 +7,11 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
-    print(f"************ {os.environ.get('DATABASE_URI')}")
+    print(f"************ {os.environ.get('DATABASE_URL')}")
     MAIL_SERVER= 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+# DATABASE_URI=postgresql://postgres:harsh2022@localhost:5432/blogapp
